858b69f7cc6b81775a371ed21ef0822898cc9278
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-22 17:42:05 +0200

    Analyse+correction données

--- backend/sql/filling_database/fill_db.php
+++ backend/sql/filling_database/fill_db.php
@@ -9,11 +9,18 @@
             echo "<th>".$data[$c]."</th>";
         }
         echo "</tr></thead><tbody>";
-        
+        $nomGroup = [];
+        $nomAlim = [];
         while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
             $num = count($data);
             $row++;
             echo "<tr>";
+            if (!in_array($data[0],$nomGroup)){
+                $nomGroup[] = $data[0];
+            }
+            if (!in_array($data[1],$nomAlim)){
+                $nomAlim[] = $data[1];
+            }
             for ($c=0; $c < $num; $c++) {
                 echo "<td>".$data[$c]."</td>";
 
@@ -21,7 +28,18 @@
             echo "</tr>";
         }
         echo "</tbody></table>";
+        $num2 = count($nomGroup);
+        echo "<p>".$num2."</p>";
+        for ($c=0; $c < $num2; $c++) {
+            echo "<p>".$nomGroup[$c]."</p>";
 
+        }
+        $num3 = count($nomAlim);
+        echo "<p>".$num3."</p>";
+        echo "<p>".$nomAlim[0]."</p>";
+        echo "<p>".$nomAlim[3150]."</p>";
+        echo "<p>".$nomAlim[3170]."</p>";
+        echo "<p>".$nomAlim[$num3-1]."</p>";
         fclose($handle);
     }
 
