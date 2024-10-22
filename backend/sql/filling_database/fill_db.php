b720cd850abefe3e73ee9aab16ec965fbe4dae07
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-22 17:08:42 +0200

    Affichage CSV

--- backend/sql/filling_database/fill_db.php
+++ backend/sql/filling_database/fill_db.php
@@ -2,15 +2,25 @@
     require_once('../../init_pdo.php');
     $row = 1;
     if (($handle = fopen("alimentsCropped.csv", "r")) !== FALSE) {
+        $data = fgetcsv($handle, 1000, ";");
+        $num = count($data);
+        echo "<table id=\"myTable\" class=\"display\"><thead><tr>";
+        for ($c=0; $c < $num; $c++){
+            echo "<th>".$data[$c]."</th>";
+        }
+        echo "</tr></thead><tbody>";
+        
         while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
             $num = count($data);
-            echo "<p> $num fields in line $row: <br /></p>\n";
             $row++;
+            echo "<tr>";
             for ($c=0; $c < $num; $c++) {
-                echo $data[$c] . "<br />\n";
+                echo "<td>".$data[$c]."</td>";
 
             }
+            echo "</tr>";
         }
+        echo "</tbody></table>";
 
         fclose($handle);
     }
