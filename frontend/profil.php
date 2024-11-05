fab8c912a9e73375d88c524ca8b1a6fdad18f6a9
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-11-05 09:08:14 +0100

    start profile page

--- frontend/profil.php
+++ frontend/profil.php
@@ -12,3 +12,39 @@
 
     require_once('templates/template_sidemenu.php');
     renderSideMenuToHTML($currentPageId);
+?>
+<div class="main">
+    <div class="titre">
+        <h1>Profil</h1>
+    </div>
+    <div class="contenu">
+        <div class="login">
+            <h2>Login</h2>
+        </div>
+        <div class="pseudo">
+            <h3>Pseudo</h3>
+        </div>
+        <div class="email">
+            <p>email@email.com</p>
+        </div>
+        <div class="password">
+            <p>••••••••</p>
+        </div>
+        <div class="birthyear">
+            <p>Année</p>
+        </div>
+        <div class="sexe">
+            <p>Sexe</p>
+        </div>
+        <div class="niveauSportif">
+            <p>Niveau Sportif</p>
+        </div>
+        <div class="boutons">
+            <button>Edit</button>
+        </div>
+    </div>
+</div>
+
+<?php
+    require_once('templates/template_footer.php');
+?>
