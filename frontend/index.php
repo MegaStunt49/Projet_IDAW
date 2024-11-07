0c1506987a73bd0b4971be0e3c1a424245a81f64
Rayyze <chppleo@gmail.com>
2024-11-07 15:51:35 +0100

    index pie chart v1

--- frontend/index.php
+++ frontend/index.php
@@ -13,4 +13,27 @@
     require_once('templates/template_sidemenu.php');
     renderSideMenuToHTML($currentPageId);
 ?>
-<div id="main"></div>
+<script src="https://d3js.org/d3.v6.min.js"></script>
+<script src="js/index.js"></script>
+
+<div id="main">
+    <h1>iMangerMieux</h1>
+    <p>Bienvenue, <span id="username-holder"></span></p>
+    <div class="section">
+        <h2 class="section-title">Consommation des types d'aliments sur <span id="periode-holder">7 jours</span></h2>
+        <div class="horizontal-container">
+            <span id="aliment-type-chart" style="flex:1"></span>
+            <p style="flex:1">Au petit-déjeuner, il est conseillé de privilégier des aliments riches en fibres comme les céréales complètes, 
+                les fruits frais, et une source de protéines légères, telles que le yaourt ou les œufs. 
+                Ces aliments fournissent de l'énergie durable pour commencer la journée et aident à réguler la glycémie. 
+                Au déjeuner, une portion de légumes, une source de protéines (comme la viande maigre, le poisson ou les légumineuses), 
+                et des glucides complexes (comme le riz complet ou les pommes de terre) sont recommandés pour un apport équilibré en nutriments.
+                Enfin, pour le dîner, il est conseillé de consommer un repas léger pour faciliter la digestion avant le sommeil, 
+                avec des légumes, des protéines végétales ou animales en petite quantité, et des glucides lents.
+            </p>
+        </div>
+    </div>
+</div>
+<?php 
+    require_once("templates/template_footer.php");
+?>
